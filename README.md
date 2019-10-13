ae0f661a711682066cb74f584609fa39ee33d0c0
庞亚静 <pangyajing@ainirobot.com>
2019-10-13 11:31:19 +0800

    init

--- README.md
+++ README.md
@@ -1,68 +1,46 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+# <img src="example/logo.png" alt="Logo" width="50"> Chinese Color And Traditional Japanese Color
 
-## Available Scripts
 
-In the project directory, you can run:
+## Chinese Color
 
-### `yarn start`
+- Color data source： [Chinese Color](http://zhongguose.com/)
 
-Runs the app in the development mode.<br />
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+<img src="example/show-c.png" width="600">
 
-The page will reload if you make edits.<br />
-You will also see any lint errors in the console.
 
-### `yarn test`
+## Traditional Japanese Color
 
-Launches the test runner in the interactive watch mode.<br />
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+- Color data source： [wikipedia Traditional Japanese Color ](https://zh.wikipedia.org/wiki/日本传统色系)
 
-### `yarn build`
+<img src="example/show-j.png" width="600">
 
-Builds the app for production to the `build` folder.<br />
-It correctly bundles React in production mode and optimizes the build for the best performance.
 
-The build is minified and the filenames include the hashes.<br />
-Your app is ready to be deployed!
+## Technology Stack 
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+- [create-react-app](https://github.com/facebook/create-react-app)
+- [react.js](https://reactjs.org)
+- prettier: for better code
+- [iconfont.cn](https://iconfont.cn): icons
+- [jinrishici](https://www.jinrishici.com): get poems data 
 
-### `yarn eject`
 
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
+## Local Development
 
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
+```
+git clone https://github.com/Carrie999/chinese-colors
 
-Instead, it will copy all the configuration files and the transitive dependencies (Webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
+cd chinese-colors && npm install
 
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
+npm run start
+```
 
-## Learn More
 
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
 
-To learn React, check out the [React documentation](https://reactjs.org/).
+## Reference && Source of inspiration
 
-### Code Splitting
+- open source project：[Chinese Color](https://github.com/zerosoul/chinese-colors/blob/master/README.zh.md)
 
-This section has moved here: https://facebook.github.io/create-react-app/docs/code-splitting
 
-### Analyzing the Bundle Size
 
-This section has moved here: https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size
-
-### Making a Progressive Web App
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app
-
-### Advanced Configuration
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/advanced-configuration
-
-### Deployment
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/deployment
-
-### `yarn build` fails to minify
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
+## Reward
+<img src="example/reward.jpg" alt="reward" width="300">
--- src/App.js
+++ src/App.js
@@ -1,26 +1,255 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './assets/font/iconfont.css'
+import './App.css'
+import chineseColors from './assets/chinese-color'
+import japanColors from './assets/japan-color'
+import japanBg from './assets/japan-bg.mp3'
+import chinaBg from './assets/china-bg.mp3'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+const jinrishici = require('jinrishici')
+
+class App extends Component {
+  state = {
+    colorsInfo: chineseColors,
+    currentColor: chineseColors[0].hex,
+    currentColorName: chineseColors[0].name,
+    currentIsDark: chineseColors[0].isDark,
+    peomContentUp: '',
+    peomContentDown: '',
+    peomAuthor: '',
+    full: false,
+    country: 'china',
+    status: 'stop'
+  }
+  componentDidMount = () => {
+    this.getPeomData()
+    this.getPeomTimer = setInterval(() => {
+      this.getPeomData()
+    }, 1000 * 10)
+    document.addEventListener('fullscreenchange', () => {
+      if (!this.isFullScreenCurrently()) {
+        this.setState({ full: false })
+      }
+    })
+  }
+  getPeomData = () => {
+    jinrishici.load(res => {
+      let contentArr = res.data.content.split('，')
+      let author = res.data.origin.author
+      this.setState({
+        peomContentUp: contentArr[0] + '，',
+        peomContentDown: contentArr[1],
+        peomAuthor: author
+      })
+    })
+  }
+  handleMouseOver = item => {
+    this.setState({
+      currentColor: item.hex,
+      currentColorName: item.name,
+      currentIsDark: item.isDark
+    })
+    if (item.jia) {
+      this.setState({
+        currentJia: item.jia
+      })
+    } else {
+      this.setState({
+        currentJia: ''
+      })
+    }
+  }
+  changeNation() {
+    let country = this.state.country === 'china' ? 'japan' : 'china'
+    if (country === 'china') {
+      this.setState({
+        colorsInfo: chineseColors,
+        country: country,
+        currentColor: chineseColors[0].hex,
+        currentColorName: chineseColors[0].name,
+        currentIsDark: chineseColors[0].isDark,
+        currentJia: ''
+      })
+    }
+    if (country === 'japan') {
+      this.setState({
+        colorsInfo: japanColors,
+        country: country,
+        currentColor: japanColors[0].hex,
+        currentColorName: japanColors[0].name,
+        currentIsDark: japanColors[0].isDark,
+        currentJia: japanColors[0].jia
+      })
+    }
+    if (this.state.status === 'play') {
+      this.stop()
+    }
+  }
+  isFullScreenCurrently = () => {
+    var full_screen_element =
+      document.fullscreenElement ||
+      document.webkitFullscreenElement ||
+      document.mozFullScreenElement ||
+      document.msFullscreenElement ||
+      null
+    // If no element is in full-screen
+    if (full_screen_element === null) return false
+    else return true
+  }
+  fullScreen = () => {
+    this.setState({ full: !this.state.full }, () => {
+      if (this.state.full) {
+        const i = this.refs.containerWrap
+        if (i.requestFullscreen) {
+          i.requestFullscreen()
+        } else if (i.webkitRequestFullscreen) {
+          i.webkitRequestFullscreen()
+        } else if (i.mozRequestFullScreen) {
+          i.mozRequestFullScreen()
+        } else if (i.msRequestFullscreen) {
+          i.msRequestFullscreen()
+        }
+      } else {
+        if (document.exitFullscreen) {
+          document.exitFullscreen()
+        } else if (document.webkitExitFullscreen) {
+          document.webkitExitFullscreen()
+        } else if (document.mozCancelFullScreen) {
+          document.mozCancelFullScreen()
+        } else if (document.msExitFullscreen) {
+          document.msExitFullscreen()
+        }
+      }
+    })
+  }
+  play = () => {
+    // 回到初始位置
+    const scrollZone = document.getElementById('scroll')
+    scrollZone.scrollTop = 0
+
+    this.setState({ status: 'play' })
+    let colorsInfo = this.state.colorsInfo,
+        i = 1
+    this.timer = setInterval(() => {
+      this.setState({
+        currentColor: colorsInfo[i].hex,
+        currentColorName: colorsInfo[i].name,
+        currentIsDark: colorsInfo[i].isDark
+      })
+      if (colorsInfo[i].currentJia) {
+        this.setState({
+          currentJia: colorsInfo[i].currentJia
+        })
+      }
+      i++
+      if (!colorsInfo[i]) i = 1
+      // 大于 6 ，右边滚动到视野区域
+      if(i>6){
+        const scrollZone = document.getElementById('scroll')
+        const setScrollHeight = (i-6)*60
+        if (scrollZone) {
+          scrollZone.scrollTop = setScrollHeight
+        }
+      }
+    }, 5000)
+  }
+  stop = () => {
+    this.setState({ status: 'stop' })
+    clearInterval(this.timer)
+  }
+  render() {
+    const {
+      currentColorName,
+      currentIsDark,
+      currentColor,
+      peomContentUp,
+      peomContentDown,
+      peomAuthor,
+      colorsInfo
+    } = this.state
+    return (
+      <div
+        className="App"
+        ref="containerWrap"
+        style={{ backgroundColor: currentColor }}
+      >
+        <audio
+          src={this.state.country === 'japan' ? japanBg : chinaBg}
+          autoPlay="autoplay"
+          loop="loop"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          Your browser does not support the audio element.
+        </audio>
+
+        <div onClick={this.fullScreen} className="full-screen">
+          {!this.state.full ? (
+            <i
+              className={'iconfont iconquanpingzuidahua iconfont-full-screen'}
+            ></i>
+          ) : (
+            <i
+              className={'iconfont icontuichuquanping2 iconfont-full-screen'}
+            ></i>
+          )}
+        </div>
+        <div className="button-span" onClick={this.changeNation.bind(this)}>
+          <span className="button-text noselect">
+            {this.state.country === 'china' ? '中' : '日'}
+          </span>
+        </div>
+        {this.state.status === 'stop' ? (
+          <i
+            onClick={this.play}
+            className={'iconfont iconxunhuanbofang play'}
+          ></i>
+        ) : (
+          <i onClick={this.stop} className={'iconfont icontingzhi play'}></i>
+        )}
+
+        <div
+          className="current-name"
+          style={{ color: `${currentIsDark ? '#000' : '#fff'}` }}
+        >
+          {this.state.currentJia ? (
+            <p className="current-name-jia">{this.state.currentJia}</p>
+          ) : (
+            ''
+          )}
+          <p className="current-name-text noselect">{currentColorName}</p>
+          <p className="current-name-color">{currentColor}</p>
+        </div>
+        <div
+          className="poem"
+          style={{ color: `${currentIsDark ? '#000' : '#fff'}` }}
+        >
+          <p className="poem-up">{peomContentUp}</p>
+          <p className="poem-down">{peomContentDown}</p>
+          <p className="poem-author">-- {peomAuthor}</p>
+          {/* <p className="poem-up" id="poem_sentence_up">柳湖松岛莲花寺，</p> */}
+          {/* <p className="poem-down" id="poem_sentence_down">晚动归桡出道场。</p> */}
+        </div>
+
+        <aside className="aside" id= 'scroll'>
+          {colorsInfo.map((item, index) => (
+            <div
+              key={index}
+              className="aside-piece"
+              style={{ backgroundColor: `${item.hex}` }}
+              onMouseOver={() => this.handleMouseOver(item)}
+            >
+              <div
+                className="aside-piece-one"
+                style={{ color: `${item.isDark ? '#000' : '#fff'}` }}
+              >
+                <p className="aside-piece-name"> {item.name} </p>
+                <p className="aside-piece-color"> {item.hex}</p>
+              </div>
+            </div>
+          ))}
+        </aside>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
