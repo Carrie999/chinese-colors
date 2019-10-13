353a646377027d86e2c8bf5928135b715bb6c00d
庞亚静 <pangyajing@ainirobot.com>
2019-10-13 11:35:57 +0800

    build

---
+++ build/precache-manifest.cb869b057274aeeea554b6e8a7db6296.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fbc0d51a0b194015df8f0909ffc3236",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f3055606ea14c8c61ea7",
+    "url": "/static/css/main.523e8fed.chunk.css"
+  },
+  {
+    "revision": "a68b0e92824cb5f21230",
+    "url": "/static/js/2.0158ef1d.chunk.js"
+  },
+  {
+    "revision": "f3055606ea14c8c61ea7",
+    "url": "/static/js/main.10b30346.chunk.js"
+  },
+  {
+    "revision": "83d97cf838c74a71b465",
+    "url": "/static/js/runtime-main.ab3ec558.js"
+  },
+  {
+    "revision": "61e05d2ce0f9cb5af21bcc26fb78e070",
+    "url": "/static/media/china-bg.61e05d2c.mp3"
+  },
+  {
+    "revision": "02fa60a9bc98285c822d13b1f4130068",
+    "url": "/static/media/iconfont.02fa60a9.ttf"
+  },
+  {
+    "revision": "c9a38f49482c0171fdcfd55846a94717",
+    "url": "/static/media/iconfont.c9a38f49.svg"
+  },
+  {
+    "revision": "da8c313479860880a3d578959bb44e76",
+    "url": "/static/media/iconfont.da8c3134.eot"
+  },
+  {
+    "revision": "ecec80d3ffd5b8f39adb2dc2fe50168a",
+    "url": "/static/media/iconfont.ecec80d3.woff"
+  },
+  {
+    "revision": "6a63927166d362dccc2f247b48a77349",
+    "url": "/static/media/japan-bg.6a639271.mp3"
+  }
+]);
